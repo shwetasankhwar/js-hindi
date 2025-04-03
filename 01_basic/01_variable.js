c9828aeead896334777998ed9494a1b0b5657ae6
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-04-03 18:05:53 +0000

    variable and datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 145333
+let accountEmail = "shweta.sank@gmail.com"
+var accountPassword = "123456789"
+accountCity = "kanpur"
+
+let accountState ;
+
+//accountId = 2// not allowed
+accountEmail = "shweta783@gmail.com"
+accountPassword = "887623"
+accountCity = "mumbai"
+
+console.log(accountId);
+console.table([accountPassword,accountEmail,accountCity,accountState])
+
+
+/*
+
+prefer not to use var because of block scope and functional scope
+
+*/
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all js code as newer vrsion 
+
+//  alert(3 +3) // we are using nodejs, not browser
+
+console.log( 3 + 
+    
+    3 )//code readability should be high
+
+console.log("Hitesh")
+
+let name = "shweta" //string
+let age = 18//int
+let isLoggedIn = false//boolean
+//------* premitive datatypes
+// number => 2^53
+// bigint 
+// string => " "
+//boolean => true/false
+//null (stand alone value)==> representation of empyu value
+//undefined =>
+// symbol => unique
+// object
+// 
+// 
+
+console.log(typeof "shweta");//string
+console.log((typeof (23)));//number
+console.log(typeof null); // object
+
+console.log(typeof undefined);//undefined
+
