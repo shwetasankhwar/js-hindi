8e7f7d33c1cb54f0770a5cc5ad4262f19199f1c9
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-13 12:42:02 +0000

    conversion

---
+++ 01_basic/03_conversionOrOperation.js
@@ -0,0 +1,39 @@
+//let score =  undefined 
+//let score = true
+//let score = null
+//let score = "33abc"
+//let score = 33
+//let score = "shweta"
+
+//const {score} = req.body
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// "333" => 33
+// "33abc"=>Nan
+// true = 1; false =>0
+
+ 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//let isLoggedIn = "shweta"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//let valueInNumber = Number(score)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"shweta" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
+
