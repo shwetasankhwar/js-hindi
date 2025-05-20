4d6f934d39bd01358dd86e95fb9e102893d76da1
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-20 08:46:57 +0000

    var

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,10 +1,10 @@
-"use strict";//treat all js code as newer vrsion 
+"use strict";//treat all js code as newer version 
 
 //  alert(3 +3) // we are using nodejs, not browser
 
 console.log( 3 + 
     
-    3 )//code readability should be high
+    3 )//code readability should be high so console.log(3+3) is better
 
 console.log("Hitesh")
 
