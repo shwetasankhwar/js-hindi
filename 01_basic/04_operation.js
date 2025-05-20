d98707ed376134afec6599309b0804720ed1ae12
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-20 10:24:28 +0000

    summery

--- 01_basic/04_operation.js
+++ 01_basic/04_operation.js
@@ -1,56 +1,6 @@
-//  let value = 3
-//  let negValue = -value
- //console.log(negValue);   
+console.log("2">1);
+console.log("02">1);
 
-//  console.log(2 +2);
-//  console.log(2-2);
-//  console.log(2*2);
-//  console.log(2/2);
-//  console.log(2**3);
-//  console.log(2%3);
- 
-//  let str1 = "shweta"
-//  let str2 = "sankhwar"
-//   let str3 = str1 + str2
-//   console.log(str3);
-  
-//   console.log("1"+ 2);
-//   console.log(1+"2");
-//   console.log("1"+"2");
-//   console.log("1"+2+2);
-//   console.log(1+2+"2");
-//   console.log(1+"2"+2);
-  // console.log(+true);
-  // console.log(+"");
-  
-  // let num1, num2, num3
+console.log(02);
 
-  // num1 = num2 = num3 = 2+2
 
-  // let gameCounter =100
-  // ++gameCounter
-  // console.log(gameCounter);
-
-  // console.log(2>1);
-  // console.log(2>=1);
-  // console.log(2<1);
-  // console.log(2==1);
-  // console.log(2!=1);
-
-//   console.log("2">1);
-//   console.log("02">1);
-  
-// console.log(null>0);
-// console.log(null>=0);
-// console.log(null==0);
-
-// console.log(undefined==0);
-// console.log(undefined<=0);
-// console.log(undefined<=0);
-// console.log(undefined!=0);
-// console.log(undefined<0);
-
- // ===0
-
- console.log("2"===2);
- 
