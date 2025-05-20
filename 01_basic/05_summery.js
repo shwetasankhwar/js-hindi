c44638f2a0ffeaa441f2fe3a3aecc96251b0af4d
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-20 10:23:39 +0000

    summery

---
+++ 01_basic/05_summery.js
@@ -0,0 +1,50 @@
+// premitive datatypes- 7 catagories
+//1: string, 2: Number, 3:Boolean, 4:Null , 5:Undefined , 6:symbol, 7:Big int
+
+// const score = 100;
+// console.log(typeof score);
+
+// const scoreValue = 100.3;
+// console.log(typeof scoreValue);
+
+// //const score = false;
+
+
+
+ const id = Symbol('123')
+console.log(typeof id);
+
+ const anotherID = Symbol('123')
+console.log( typeof anotherID);
+
+// console.log(id == anotherID);
+
+// const bigNumber = 567989433448n
+// console.log(typeof bigNumber);
+
+// const heros = ["shaktman","naagraj","doga"]//array
+// console.log(typeof heros);
+
+ 
+
+// let myObj = {
+// //objects
+// name: "shweta",
+// age : 27,
+
+//  }
+//  console.log(typeof myObj);
+ 
+
+//  const myFunction = function() {
+//     console.log("Hello World");
+    
+//  }
+
+//  console.log(typeof myFunction);
+ 
+ 
+ 
+
+//Reference type(Non Premitive)--
+// Arrays, Objects, Functions
