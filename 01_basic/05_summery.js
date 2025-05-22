79e68d7ae614d763f732c4b41661e613d4cf967d
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-22 11:22:15 +0000

    summery

--- 01_basic/05_summery.js
+++ 01_basic/05_summery.js
@@ -1,6 +1,8 @@
 // premitive datatypes- 7 catagories
 //1: string, 2: Number, 3:Boolean, 4:Null , 5:Undefined , 6:symbol, 7:Big int
 
+//const { captureOwnerStack } = require("react");
+
 // const score = 100;
 // console.log(typeof score);
 
@@ -11,11 +13,11 @@
 
 
 
- const id = Symbol('123')
-console.log(typeof id);
+//  const id = Symbol('123')
+// console.log(typeof id);
 
- const anotherID = Symbol('123')
-console.log( typeof anotherID);
+//  const anotherID = Symbol('123')
+// console.log( typeof anotherID);
 
 // console.log(id == anotherID);
 
@@ -48,3 +50,32 @@ console.log( typeof anotherID);
 
 //Reference type(Non Premitive)--
 // Arrays, Objects, Functions
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+// Stack ,Heap - 2 type of memory
+// stack- (Primitive), Heap(Non Primitive)
+
+
+let myYouTubename= "Shwetayt.com"
+let anotherName = myYouTubename
+anotherName = "ChaiAndCode"
+console.log(anotherName);
+console.log(myYouTubename);
+
+
+let userOne = {
+
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="shweta@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
