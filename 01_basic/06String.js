b9016cadac19a4b85e0bb502e78ffde9f7dc73c7
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-22 12:55:07 +0000

    string

---
+++ 01_basic/06String.js
@@ -0,0 +1,39 @@
+const name ="shweta"
+const repoCount = 50
+
+// console.log(name + repoCount +"Value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String(`ShwetaSS-hc-com`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf(`t`));
+
+// const   newString =gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-1,4)
+console.log(anotherString);
+
+const thisString = gameName.slice(0,4)
+console.log(thisString);
+
+const newStringOne = "  Hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url =" https://shweta.com/shweta%20singh"
+
+
+console.log(url.replace(`%20`,`-`));
+
+console.log(url.includes(`harshit`));
+ 
+console.log(gameName.split(`-`));
+
