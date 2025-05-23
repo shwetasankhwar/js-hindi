6bfd1d26976486b9cc20484f6f57a776d0d5b304
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-23 14:27:01 +0000

    array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,40 @@
+//arrays
+
+//const myArray = [0,1,2,3,4,5,6,true, "shweta"]
+const myArray = [1,1,2,3,4,5,6]
+const myHeros = ["shaktiman","thor","ironMan"]
+const myArray2 = new Array(1,2,3,4)
+
+//console.log(myArray2[1]);
+
+//  Array methods
+
+// myArray.push(7)
+// myArray.push(8)
+// myArray.pop()
+//myArray.unshift(9)
+//myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(9));
+// console.log(myArray.indexOf(1));
+
+// const newArray=myArray.join()
+
+// console.log(myArray);
+// console.log(newArray);
+
+//++++++slice and Splice
+
+console.log("A ",myArray);
+
+const myn1 =myArray.slice(1,3)
+console.log(myn1);
+console.log("B ",myArray);
+
+const myn2 =myArray.splice(1,3)
+console.log(myn2);
+console.log("C ",myArray);
+
+
+
