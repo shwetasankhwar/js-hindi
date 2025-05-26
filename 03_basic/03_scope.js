c3243b06f26caae01da22e7308ec249675f4c656
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-05-26 10:08:55 +0000

    scope

---
+++ 03_basic/03_scope.js
@@ -0,0 +1,59 @@
+//var c = 300
+let a = 300
+if(true){
+    let a =10
+const b =20
+//var c = 30
+//console.log("INNER:",a);
+
+}
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(params) {
+    const username = "hitesh"
+    function two(params) {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+   // console.log(website);
+   // two()
+    
+}
+one()
+
+if (true) {
+    const username ="shweta"
+    if(username ==="shweta"){
+        const website ="  youtube"
+       // console.log(username+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+//+++++++++++++++++++++++++==
+
+
+console.log(addone(5))
+function addone (num) {
+    return num +1
+    
+}
+
+addTwo(5)
+const addTwo = function (num) {
+    return num +2
+
+    
+}
